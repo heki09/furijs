871e0d3eec566368d83dda1d1010bb189a48d6f4
hekitter <heki09@gmail.com>
2019-06-10 23:30:18 +0900

    function test

---
+++ chap4-2.js
@@ -0,0 +1,10 @@
+let createMail = () => {
+    console.log("hello");
+}
+createMail();
+
+let createName = (name) => {
+    console.log("Mr."+name)
+    console.log("hello");
+}
+createName("Tanaka");
---
+++ chap4-3.js
@@ -0,0 +1,6 @@
+    let createName = (name,day) => {
+        let msg=`Mr.${name}
+        Please come on ${day}`
+        console.log(msg)
+    }
+    createName("Tanaka","Monday");
